fc0c688699e5c9ea9cba1af319215648be455c30
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-10-29 19:36:18 +0100

    storing user data in app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignInSignOut from './pages/sign-in-sign-out/sign-in-sign-out.component';
 
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -23,9 +23,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot( snapShot => {
+          this.setState({
+            currentUser: {
+              id : snapShot.id,
+              ...snapShot.data()
+            }
+          }) 
+        });
+      }
+      this.setState({currentUser : userAuth});
+    });
   }
 
   componentWillUnmount () {
