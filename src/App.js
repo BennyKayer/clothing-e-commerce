6bba340fc36651b2825f0428002042908e756ca7
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-10-29 10:11:43 +0100

    Added collection frontend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route exact path='/hats' component = {HatsPage}/>
+        <Route exact path='/shop' component = {ShopPage}/>
       </Switch>
       
     </div>
