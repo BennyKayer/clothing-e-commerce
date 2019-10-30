7efb4fc967a896103b4192cf42dc1f1deb43c979
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-10-30 22:23:57 +0100

    Finished sign-in-sign-out

--- src/App.js
+++ src/App.js
@@ -33,8 +33,7 @@ class App extends React.Component {
               id : snapShot.id,
               ...snapShot.data()
             }
-          }); 
-          console.log(this.state);
+          });
         });
       }
       this.setState({ currentUser : userAuth });
