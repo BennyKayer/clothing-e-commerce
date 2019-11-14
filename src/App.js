ab604019f77f8271a15677766c0a3fa329d98dc2
Paweł Benkowski <pawlo97.pb@gmail.com>
2019-11-14 17:52:47 +0100

    updating app to redirect to home when user signs out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -46,7 +46,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage}/>
           <Route exact path='/shop' component = {ShopPage}/>
-          <Route exact path='/signin' component={SignInSignOut}/>
+          <Route exact path='/signin' render = {
+            () => this.props.currentUser ?
+             (<Redirect to='/'/>) : <SignInSignOut/>}/>
         </Switch>
         
       </div>
@@ -54,8 +56,11 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
